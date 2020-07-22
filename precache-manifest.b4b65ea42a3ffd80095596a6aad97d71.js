6e0768f7cbb350600afc46b4a8bc65f42b26dc07
ariktwena <ariktwena@gmail.com>
2020-07-22 13:11:22 +0200

    Updates

---
+++ precache-manifest.b4b65ea42a3ffd80095596a6aad97d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df118cd117effba594381575081b1bd",
+    "url": "/monster-live-2/index.html"
+  },
+  {
+    "revision": "a8bb54364075c47f0875",
+    "url": "/monster-live-2/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "85cfd3dc938699e44355",
+    "url": "/monster-live-2/static/js/2.46dde941.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-live-2/static/js/2.46dde941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8bb54364075c47f0875",
+    "url": "/monster-live-2/static/js/main.47d41e3e.chunk.js"
+  },
+  {
+    "revision": "d6ee73ca7a17569a9a96",
+    "url": "/monster-live-2/static/js/runtime-main.50b2bca5.js"
+  }
+]);
